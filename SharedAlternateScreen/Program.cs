0f1c4c0a27bf76bbb3b782b333a224b7f2a8fb02
Oghenetejiritop <tjsouthernking@gmail.com>
2024-09-18 08:57:45 -0400

    Add project files.

---
+++ SharedAlternateScreen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedAlternateScreen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string displayName = "desktop";
+            Console.WriteLine("Currently set to {0} Screen.", displayName);
+
+        }
+    }
+}
